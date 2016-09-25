b2bc78a1b98fc150dcb4c6bac41cb397615be8d8
Vinod Vishwanath <vinod.v@redbus.in>
2016-09-26 03:15:25 +0530

    Added web-view to view-controller

--- Landmark/ViewController.swift
+++ Landmark/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var webView : UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController : UIWebViewDelegate {
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+        
+        
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        return true
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+    
+    }
 }
 
